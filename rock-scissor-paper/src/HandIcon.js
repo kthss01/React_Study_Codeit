202677d0b1c01eb011534c9f3c38f17cdb525a9e
kthss01 <kthss01@naver.com>
2022-02-26 06:17:43 +0900

    CSS 클래스네임 적용

--- rock-scissor-paper/src/HandIcon.js
+++ rock-scissor-paper/src/HandIcon.js
@@ -8,9 +8,9 @@ const IMAGES = {
     paper : paperImg
 };
 
-function HandIcon({ value = 'rock' }) {
+function HandIcon({ className, value = 'rock' }) {
     const src = IMAGES[value];
-    return <img src={src} alt={value} />;
+    return <img className={className} src={src} alt={value} />;
 }
 
 export default HandIcon;
