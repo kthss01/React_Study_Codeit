2f182f5d94f69f64e5ed63ae33d4f4fe35735653
kthss01 <kthss01@naver.com>
2022-03-14 08:35:36 +0900

    fetch 사용하기

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,10 +1,10 @@
 import FoodList from "./FoodList";
-import mockItems from "../mock.json";
 import { useState } from "react";
+import { getFoods } from "../api";
 
 function App() {
     const [order, setOrder] = useState("createAt");
-    const [items, setItems] = useState(mockItems);
+    const [items, setItems] = useState([]);
     const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
     const handleNewestClick = () => setOrder("createdAt");
@@ -13,14 +13,17 @@ function App() {
         const nextItems = items.filter((item) => item.id !== id);
         setItems(nextItems);
     };
+    const handleLoadClick = async () => {
+        const { foods } = await getFoods();
+        setItems(foods);
+    };
 
     return (
         <div>
-            <div>
-                <button onClick={handleNewestClick}>최신순</button>
-                <button onClick={handleCalorieClick}>칼로리순</button>
-            </div>
+            <button onClick={handleNewestClick}>최신순</button>
+            <button onClick={handleCalorieClick}>칼로리순</button>
             <FoodList items={sortedItems} onDelete={handleDelete} />
+            <button onClick={handleLoadClick}>불러오기</button>
         </div>
     );
 }
