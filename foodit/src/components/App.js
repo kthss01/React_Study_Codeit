1ada5d2fa6866d5c70b49fe5859d196b24253cdb
kthss01 <kthss01@naver.com>
2022-03-14 20:24:19 +0900

    useEffect

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,29 +1,35 @@
 import FoodList from "./FoodList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getFoods } from "../api";
 
 function App() {
-    const [order, setOrder] = useState("createAt");
+    const [order, setOrder] = useState("createdAt");
     const [items, setItems] = useState([]);
-    const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
     const handleNewestClick = () => setOrder("createdAt");
     const handleCalorieClick = () => setOrder("calorie");
+
     const handleDelete = (id) => {
         const nextItems = items.filter((item) => item.id !== id);
         setItems(nextItems);
     };
-    const handleLoadClick = async () => {
-        const { foods } = await getFoods();
+
+    const handleLoad = async (orderQuery) => {
+        const { foods } = await getFoods(orderQuery);
         setItems(foods);
     };
+
+    const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+    useEffect(() => {
+        handleLoad(order);
+    }, [order]);
 
     return (
         <div>
             <button onClick={handleNewestClick}>최신순</button>
             <button onClick={handleCalorieClick}>칼로리순</button>
             <FoodList items={sortedItems} onDelete={handleDelete} />
-            <button onClick={handleLoadClick}>불러오기</button>
         </div>
     );
 }
--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -1,6 +1,6 @@
 import ReviewList from "./ReviewList";
 // import mockItems from "../mock.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 
 function App() {
@@ -17,10 +17,21 @@ function App() {
         setItems(nextItems);
     };
 
-    const handleLoadClick = async () => {
-        const { reviews } = await getReviews();
+    const handleLoad = async (orderQuery) => {
+        const { reviews } = await getReviews(orderQuery);
         setItems(reviews);
     };
+
+    //handleLoad(); // 무한루프 발생함
+
+    // const handleLoadClick = async () => {
+    //     const { reviews } = await getReviews();
+    //     setItems(reviews);
+    // };
+
+    useEffect(() => {
+        handleLoad(order);
+    }, [order]);
 
     return (
         <div>
@@ -29,7 +40,7 @@ function App() {
                 <button onClick={handleBestClick}>베스트순</button>
             </div>
             <ReviewList items={sortedItems} onDelete={handleDelete} />
-            <button onClick={handleLoadClick}>불러오기</button>
+            {/* <button onClick={handleLoadClick}>불러오기</button> */}
         </div>
     );
 }
