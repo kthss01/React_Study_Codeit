79c47c2ce3edc62e1f85d779005a0513cda00e9d
kthss01 <kthss01@naver.com>
2022-03-29 08:38:56 +0900

    사이드 이펙트, useEffect, 이미지 파일 미리보기, 타이머 예제

--- moviepedia/src/components/FileInput.js
+++ moviepedia/src/components/FileInput.js
@@ -1,9 +1,10 @@
-// import { useState } from "react";
-
+import { useState } from "react";
 import { useEffect, useRef } from "react";
 
 function FileInput({ name, value, onChange }) {
     // const [value, setValue] = useState("");
+
+    const [preview, setPreview] = useState();
 
     const inputRef = useRef();
 
@@ -15,13 +16,43 @@ function FileInput({ name, value, onChange }) {
         onChange(name, nextValue);
     };
 
-    // useEffect(() => {
-    //     if (inputRef.current) {
-    //         console.log(inputRef);
-    //     }
-    // }, []);
+    useEffect(() => {
+        // if (inputRef.current) {
+        //     console.log(inputRef);
+        // }
 
-    return <input type="file" onChange={handleChange} ref={inputRef} />;
+        if (!value) return;
+
+        // 해당 파일의 주소처럼 쓸 수 있는 값 문자열로 리턴
+        const nextPreview = URL.createObjectURL(value);
+        setPreview(nextPreview);
+
+        return () => {
+            setPreview();
+            URL.revokeObjectURL(nextPreview);
+        };
+    }, [value]);
+
+    const hanldeClearClick = () => {
+        const inputNode = inputRef.current;
+        if (!inputNode) return;
+
+        inputNode.value = "";
+        onChange(name, null);
+    };
+
+    return (
+        <div>
+            <img src={preview} alt="이미지 미리보기" />
+            <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleChange}
+                ref={inputRef}
+            />
+            {value && <button onClick={hanldeClearClick}>X</button>}
+        </div>
+    );
     // return <input type="file" value={value} onChange={handleChange} />;
 }
 
